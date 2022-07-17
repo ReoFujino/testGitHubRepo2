0031bf5ac278bde0639e2f0b41798d21fb873138
rfujino <rfujino@MACBOOK-AIR-RF>
2022-07-17 14:24:30 +0900

    初期コミット

---
+++ testGitBranchProj/src/testGitBranchProj/testGitBranchModule.java
@@ -0,0 +1,20 @@
+/**
+ *
+ */
+package testGitBranchProj;
+
+/**
+ * @author rfujino
+ *
+ */
+public class testGitBranchModule {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//XXX 初期コミット
+		System.out.print("START");
+	}
+
+}
